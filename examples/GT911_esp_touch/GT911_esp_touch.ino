c66cfeccf815bf8f869b5d30e891c9e69764ac7c
andrew911 <39950568+u4mzu4@users.noreply.github.com>
2021-02-24 21:14:24 +0100

    add GT911 ESP example

--- examples/GT911_esp_touch/GT911_esp_touch.ino
+++ examples/GT911_esp_touch/GT911_esp_touch.ino
@@ -2,37 +2,41 @@
 #include "Goodix.h"
 
 // D5 D6 pins are defined on boards like WeMOS, You can redefine it to numeric values
-#define INT_PIN D6
-#define RST_PIN D5
-
+#define INT_PIN 34
+#define RST_PIN -1
+#define LILYGO_LILYPI true
 
 Goodix touch = Goodix();
 
-void handleTouch(int8_t contacts, GTPoint *points) {
+void handleTouch(int8_t contacts, GTPoint* points) {
   Serial.printf("Contacts: %d\n", contacts);
   for (uint8_t i = 0; i < contacts; i++) {
-    Serial.printf("C%d: #%d %d,%d s:%d\n", i, points[i].trackId, points[i].x, points[i].y, points[i].area);
+    Serial.printf("C%d: %d %d \n", i, points[i].x, points[i].y);
     yield();
   }
 }
 
 void touchStart() {
- if (touch.begin(INT_PIN, RST_PIN)!=true) {
-    Serial.println("! Module reset failed");
-  } else {
-    Serial.println("Module reset OK");
-  }
+  unsigned short configInfo;
   
+  touch.begin(INT_PIN, RST_PIN, GOODIX_I2C_ADDR_BA);
   Serial.print("Check ACK on addr request on 0x");
   Serial.print(touch.i2cAddr, HEX);
-  
-  Wire.beginTransmission(touch.i2cAddr);  
-  int error = Wire.endTransmission();
-  if (error == 0) {    
-    Serial.println(": SUCCESS");   
+  Wire.beginTransmission(touch.i2cAddr);
+  if (!Wire.endTransmission()) {
+    Serial.println(": SUCCESS");
   } else {
-    Serial.print(": ERROR #");
-    Serial.println(error);
+    Serial.print(": ERROR!");
+  }
+  configInfo = touch.configCheck(LILYGO_LILYPI);
+  if (!configInfo)
+  {
+    Serial.println("Config is OK");
+  }
+  else
+  {
+    Serial.print("Config ERROR: ");
+    Serial.println(configInfo);
   }
 }
 
@@ -46,9 +50,10 @@ void setup() {
 
   touch.setHandler(handleTouch);
   touchStart();
+  //touch.configUpdate();
 }
 
-void loop() {  
+void loop() {
   touch.loop();
-  delay(1);
+  delay(10);
 }
